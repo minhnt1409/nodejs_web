a81c9731908d561bee63a2d6d7d6e53e71402964
minhnt1409 <minh.nt200409@gmail.com>
2023-03-02 05:26:34 +0700

    xong

--- src/app/controllers/RoomController.js
+++ src/app/controllers/RoomController.js
@@ -12,15 +12,15 @@ class RoomController {
     }
 
     store(req, res, next) {
-        
-        if(req.body.submit == 'create'){
+
+        if (req.body.submit == 'create') {
             const room = new Room(req.body);
             room.save({})
                 .then(res.redirect('/'))
                 .catch(next);
         }
-        if(req.body.submit == 'edit'){
-            Room.findOneAndUpdate({id_phong: req.body.id_phong}, req.body)
+        if (req.body.submit == 'edit') {
+            Room.findOneAndUpdate({ id_phong: req.body.id_phong }, req.body)
                 .then(res.redirect('/'))
                 .catch(next);
         }
@@ -35,28 +35,28 @@ class RoomController {
         var now = new Date();
         var datetime = now.toString().replace(' GMT+0700 (Indochina Time)', '');
         console.log(req.body);
-        if(req.body.submit == 'nhan'){
+        if (req.body.submit == 'nhan') {
             req.body.check_in = datetime;
             req.body.trang_thai = "hired";
             const history = new History(req.body);
             history.save({}).then(console.log("add history"));
-        }else{
+        } else {
             req.body.trang_thai = "ready";
             req.body.so_nguoi_hien_tai = null;
             req.body.ten_nguoi_dai_dien = null;
             req.body.id_nguoi_dai_dien = null;
-            History.findOneAndUpdate({check_in: req.body.check_in}, {check_out: datetime, phuong_thuc_thue: req.body.phuong_thuc_thue ,thanh_toan: req.body.pay}).then(console.log("update history"));
+            History.findOneAndUpdate({ check_in: req.body.check_in }, { check_out: datetime, phuong_thuc_thue: req.body.phuong_thuc_thue, thanh_toan: req.body.pay }).then(console.log("update history"));
             req.body.check_in = null;
             req.body.phuong_thuc_thue = null;
         }
-        
-        Room.findOneAndUpdate({id_phong: req.body.id_phong}, req.body)
+
+        Room.findOneAndUpdate({ id_phong: req.body.id_phong }, req.body)
             .then(res.redirect('/rooms/' + req.body.id_phong))
             .catch(next);
     }
 
     edit(req, res, next) {
-        console.log(req.params.slug );
+        console.log(req.params.slug);
         Room.findOne({ id_phong: req.params.slug })
             .then((rooms) => {
                 res.render('rooms/edit', rooms);
@@ -66,7 +66,7 @@ class RoomController {
 
     delete(req, res, next) {
         console.log(req.params.slug);
-        Room.findOneAndDelete({id_phong: req.params.id})
+        Room.findOneAndDelete({ id_phong: req.params.id })
             .then(res.redirect('/'))
             .catch(next);
     }
--- src/app/models/History.js
+++ src/app/models/History.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const History = new Schema({
-    id_phong: { type: Number},
-    ten_phong: { type: String, required: true},
+    id_phong: { type: Number },
+    ten_phong: { type: String, required: true },
     ten_nguoi_dai_dien: { type: String },
     id_nguoi_dai_dien: { type: Number },
-    phuong_thuc_thue: { type: String, default: null},
+    phuong_thuc_thue: { type: String, default: null },
     thanh_toan: { type: String, default: null },
-    check_in: {type: String},
-    check_out: {type: String, default: null },
+    check_in: { type: String },
+    check_out: { type: String, default: null },
 });
 
 export default mongoose.model('History', History);
